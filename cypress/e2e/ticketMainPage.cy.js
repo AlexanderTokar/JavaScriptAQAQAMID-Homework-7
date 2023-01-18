40e3b5fa6dc7588abde75149356b0857173877a9
AlexanderTokar <alextokar@inbox.ru>
2023-01-18 19:22:41 +0300

    added some tests

--- cypress/e2e/ticketMainPage.cy.js
+++ cypress/e2e/ticketMainPage.cy.js
@@ -1,6 +1,43 @@
 describe('Movie tickets tests', () => {
-    it('First test - positive', async () => {
-        cy.visit('qamid.tmweb.ru');
-        cy.get('.page-nav__day').should('have.length', 7);
+    const movie = require('../fixtures/bookingData.json');
+    const seats = require('../fixtures/seats.json');
+    const days = require('../fixtures/days.json');
+
+    let randomDay;
+    let randomSeat;
+
+    beforeEach(() => {
+        randomDay = [Math.floor(Math.random() * days.length)];
+        cy.visit(movie.address);
+        cy.get(days[randomDay].day).click();
+        cy.get(movie.film).first().contains('23:45').click();
+    });
+
+    it('First test (should book one ticket) - positive', async () => {
+        cy.get(seats[0].seat).click();
+        cy.get(movie.accept, { timeout: 10000 }).click();
+        cy.contains('Вы выбрали билеты:').should('be.visible');
+    });
+
+    it.skip('Second test (should book two tickets) - positive', async () => {
+        cy.get(seats[2].seat).click();
+        cy.get(seats[3].seat).click();
+        cy.get(movie.accept, { timeout: 10000 }).click();
+        cy.contains('Вы выбрали билеты:').should('be.visible');
+    });
+
+    it.skip('Third test (should not book ticket chosen twice) - negative', async () => {
+        randomSeat = [Math.floor(Math.random() * seats.length)];
+        cy.log(randomSeat);
+        cy.get(seats[4].seat).click();
+        cy.log(randomSeat);
+        cy.get(seats[4].seat).click();
+        cy.get(movie.accept).should('be.visible');
+    });
+
+    it.skip('Fourth test (should not select non active time) - negative', async () => {
+        cy.visit(movie.address);
+        cy.get(days[0].day).click();
+        cy.get(movie.filmNonActive).should('be.visible');
     });
 });
